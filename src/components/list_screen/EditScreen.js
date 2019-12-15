ec18682ed7816befa26dd241f3a3d95537d15df0
Andrew <andrewkong101999@gmail.com>
2019-12-15 02:55:26 -0500

    Fixed getting element error

--- src/components/list_screen/EditScreen.js
+++ src/components/list_screen/EditScreen.js
@@ -259,10 +259,10 @@ class EditScreen extends Component {
             this.state.items[index].positionX = this.state.x;
             this.state.items[index].positionY = this.state.y;
         }
-        document.getElementById(this.state.selectedItem.id).children[0].style.opacity = 1;
-        document.getElementById(this.state.selectedItem.id).children[1].style.opacity = 1;
-        document.getElementById(this.state.selectedItem.id).children[2].style.opacity = 1;
-        document.getElementById(this.state.selectedItem.id).children[3].style.opacity = 1;
+        this.state.selectedItem.children[0].style.opacity = 1;
+        this.state.selectedItem.children[1].style.opacity = 1;
+        this.state.selectedItem.children[2].style.opacity = 1;
+        this.state.selectedItem.children[3].style.opacity = 1
     }
     }
 
@@ -458,16 +458,16 @@ class EditScreen extends Component {
     select(e) {
         e.preventDefault();
         if(this.state.selectedItem != null){
-            document.getElementById(this.state.selectedItem.id).children[0].style.opacity = 0;
-            document.getElementById(this.state.selectedItem.id).children[1].style.opacity = 0;
-            document.getElementById(this.state.selectedItem.id).children[2].style.opacity = 0;
-            document.getElementById(this.state.selectedItem.id).children[3].style.opacity = 0;
+            this.state.selectedItem.children[0].style.opacity = 0;
+            this.state.selectedItem.children[1].style.opacity = 0;
+            this.state.selectedItem.children[2].style.opacity = 0;
+            this.state.selectedItem.children[3].style.opacity = 0;
         }
         this.state.selectedItem = e.target;
-        e.target.children[0].style.opacity = 1;
-        e.target.children[1].style.opacity = 1;
-        e.target.children[2].style.opacity = 1;
-        e.target.children[3].style.opacity = 1;
+        this.state.selectedItem.children[0].style.opacity = 1;
+        this.state.selectedItem.children[1].style.opacity = 1;
+        this.state.selectedItem.children[2].style.opacity = 1;
+        this.state.selectedItem.children[3].style.opacity = 1;
         var item = this.state.selectedItem
         var fontSize = item.style.fontSize;
         var width = item.style.borderWidth;
@@ -504,10 +504,10 @@ class EditScreen extends Component {
 
     unselect(e) {
         if(e.target.id == "old_container" && this.state.selectedItem != null){
-        document.getElementById(this.state.selectedItem.id).children[0].style.opacity = 0;
-        document.getElementById(this.state.selectedItem.id).children[1].style.opacity = 0;
-        document.getElementById(this.state.selectedItem.id).children[2].style.opacity = 0;
-        document.getElementById(this.state.selectedItem.id).children[3].style.opacity = 0;
+            this.state.selectedItem.children[0].style.opacity = 0;
+            this.state.selectedItem.children[1].style.opacity = 0;
+            this.state.selectedItem.children[2].style.opacity = 0;
+            this.state.selectedItem.children[3].style.opacity = 0;
         this.setState({
             selectedItem: null,
             type: "",
@@ -633,15 +633,14 @@ class EditScreen extends Component {
     selectItem(e){
         e.preventDefault();
         if(this.state.selectedItem != null && this.state.selectedItem.id != e.target.id){
-            console.log('hello1');
-            document.getElementById(this.state.selectedItem.id).children[0].style.opacity = 0;
-            document.getElementById(this.state.selectedItem.id).children[1].style.opacity = 0;
-            document.getElementById(this.state.selectedItem.id).children[2].style.opacity = 0;
-            document.getElementById(this.state.selectedItem.id).children[3].style.opacity = 0;
+            this.state.selectedItem.children[0].style.opacity = 0;
+            this.state.selectedItem.children[1].style.opacity = 0;
+            this.state.selectedItem.children[2].style.opacity = 0;
+            this.state.selectedItem.children[3].style.opacity = 0;
             this.setState({
                 selectedItem: e.target})
         }
-        else if(this.state.selectedItem == null){
+        else if(this.selectedItem == null){
             this.setState({
                 selectedItem: e.target})
         }
